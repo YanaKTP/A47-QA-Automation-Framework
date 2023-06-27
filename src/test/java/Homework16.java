a5912066c38ced61cadcb57c9647baa19933b54b
YanaKTP <137319268+YanaKTP@users.noreply.github.com>
2023-06-26 20:21:30 -0600

    Create Homework16.java

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest {
+
+    public void registrationNavigation(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        WebElement regestration = driver.findElement(By.cssSelector("[id='hel']"));
+        regestration.click();
+
+
+        WebElement submitButton = driver.findElement(By.cssSelector("[id='button']"));
+
+        Assert.assertTrue(submitButton.isDisplayed());
+        driver.quit();
+
+
+
+    }
+
+
+}
