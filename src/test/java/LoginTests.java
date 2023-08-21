3946d0759e00cd93be9d849537962e9c358f2971
YanaKTP <137319268+YanaKTP@users.noreply.github.com>
2023-08-21 15:57:09 -0600

    POM
    POM

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,3 +1,5 @@
+import Pages.HomePage;
+import Pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,18 +10,26 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void LoginEmptyEmailPasswordTest() {
+//    public void LoginEmptyEmailPasswordTest() {
+//
+////      Added ChromeOptions argument below to fix websocket error
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+//
+//        WebDriver driver = new ChromeDriver(options);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//        String url = "https://qa.koel.app/";
+//        driver.get(url);
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+//        driver.quit();
+//    }
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+    public void loginValidPassword(){
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+        loginPage.login();
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
     }
 }
---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,32 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage{
+    public LoginPage(WebDriver givenDriver){
+        super(givenDriver);
+    }
+
+    By emailField = By.cssSelector("[type = 'email']");
+    By passwordField = By.cssSelector("[type = 'password']");
+    By submitBTN = By.cssSelector("button[type = 'submit']");
+
+    public void provideEmail(String email){
+        findElement(emailField).sendKeys(email);
+
+    }
+    public void providePassword(String password){
+        findElement(passwordField).sendKeys(password);
+    }
+    public void clickSubmit(){
+        findElement(submitBTN).click();
+    }
+
+    public void login(){
+        provideEmail("yana.kurenko@testpro.io");
+        providePassword("te$t$tudent");
+        clickSubmit();
+    }
+}
+
