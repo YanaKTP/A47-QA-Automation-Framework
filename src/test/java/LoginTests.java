d92f446db70ee9adcd870e5151c3386de16f065d
YanaKTP <137319268+YanaKTP@users.noreply.github.com>
2023-08-22 21:15:11 -0600

    POM
    POM1

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -14,6 +14,7 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
+    
     public void loginValidPassword(){
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
